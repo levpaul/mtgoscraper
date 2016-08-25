a1144520db346a9b8bed22dc04da25ea0474cd5d
Levi Lovelock <lovelock.levi@gmail.com>
2016-08-24 22:59:49 -0700

    Added rough plan and func signitures for parser

--- parser/parser.go
+++ parser/parser.go
@@ -7,10 +7,23 @@ import (
 )
 
 // Parser A parser of webpages to pull decklists
-type Parser struct{}
+type Parser struct {
+	responseBody []byte
+}
 
 // ParseEvent is a function that will parse an entire magic event
 // from the MTGO website, given a url
 func (p *Parser) ParseEvent(url string) (*magic.Event, error) {
+	// Grab html from url - stores response body of event in parser
+	// func (p *Parser) fetchEventFromURL() (error)
+
+	// Parse event title from stored responseBody
+	// func (p *Parser) parseEventTitle() (string, error)
+
+	// Isolate/extract decklists html from responseBody
+	// func (p *Parser) extractDeckLists() ([]magic.Deck, error)
+
+	// Locally compose a magic.Event and return
+
 	return nil, errors.New("ParseEvent has not been implemented yet!")
 }
